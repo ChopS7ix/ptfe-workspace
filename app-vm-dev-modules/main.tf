696f3763804fc2ea3ad6f1d32d573f64afdabcfe
ChopS7ix <57545600+ChopS7ix@users.noreply.github.com>
2020-03-26 11:38:22 -0400

    Create main.tf

---
+++ app-vm-dev-modules/main.tf
@@ -0,0 +1,26 @@
+# Provider Credentials can be loaded via
+# export VSPHERE_SERVER=""
+# export VSPHERE_USER=""
+# export VSPHERE_PASSWORD=""
+provider "vsphere" {
+  allow_unverified_ssl = true
+}
+
+locals {
+  datacenter_name      = "Datacenter"
+  cluster_name         = "East"
+  datastore_name       = "370SSDDatastore1"
+  network_name         = "VM Network"
+  virtual_machine_name = "DB_MODVM"
+}
+
+module "vm" {
+  source  = "TFE_HOSTNAME/YOUR_ORG_NAME/vm/vsphere"
+  version = "0.0.1"
+
+  datacenter_name      = local.datacenter_name
+  cluster_name         = local.cluster_name
+  datastore_name       = local.datastore_name
+  network_name         = local.network_name
+  virtual_machine_name = local.virtual_machine_name
+}
